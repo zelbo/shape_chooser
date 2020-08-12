4c8835b2b0299def463ecdbaa23170c11075fb58
zelbo <oesvelvet@hotmail.com>
2020-08-11 20:38:58 -0400

    Add files via upload
    minor tinkering

--- paths.py
+++ paths.py
@@ -6,13 +6,16 @@ class PathClass:
     relative_path = os.path.dirname(__file__)
     debug_path = os.path.join(relative_path, "debug")
     data_path = os.path.join(relative_path, "data")
+    image_path = os.path.join(relative_path, "images")
+    if not os.path.exists(image_path):
+        os.makedirs(image_path)
     shape_path = os.path.join(data_path, "shapes")
     covid_path = os.path.join(data_path, "covid")
-    merge_path = os.path.join(data_path, "merge")
+    election_results_path = os.path.join(data_path, "election_results")
 
     # -Files-
     # TODO: don't like this at all. How to declare a blank path?
-    no_file = os.path.join(data_path, "blank.csv")
+    no_file = os.path.join(data_path, "")
     election_results = no_file
     glue_file = no_file
     political_file = no_file
@@ -26,17 +29,16 @@ class PathClass:
     county_covid_cases_file = no_file
     county_covid_deaths_file = no_file
 
-    def __init__(self, detail_level):
-        #nonlocal relative_path, merge_path, data_path, debug_path, shape_path, covid_path
-        #nonlocal election_results, glue_file, political_file, covid_political_file, debug_file, state_shapefile, county_shapefile, state_covid_cases_file, state_covid_deaths_file, county_covid_cases_file, county_covid_deaths_file
-        PathClass.election_results = os.path.join(PathClass.merge_path, detail_level + "_results.csv")
-        PathClass.glue_file = os.path.join(PathClass.merge_path, detail_level + "_glue.csv")
+    def __init__(self, detail_level, data_option):
+        detail_covid = detail_level + "_covid_" + data_option
+        PathClass.election_results = os.path.join(PathClass.election_results_path, detail_level + "_results.csv")
+        PathClass.glue_file = os.path.join(PathClass.election_results_path, detail_level + "_glue.csv")
         PathClass.political_file = os.path.join(PathClass.data_path, detail_level + "_political.csv")
-        PathClass.covid_political_file = os.path.join(PathClass.data_path, detail_level + "_covid_political.csv")
-        PathClass.debug_file = os.path.join(PathClass.debug_path, detail_level + "_merge_debug_data.csv")
+        PathClass.covid_political_file = os.path.join(PathClass.data_path, detail_covid + "_political.csv")
+        PathClass.debug_file = os.path.join(PathClass.debug_path, detail_covid + "_debug_data.csv")
 
-        PathClass.state_shapefile = os.path.join(PathClass.shape_path, "s_11au16.shp")
-        PathClass.county_shapefile = os.path.join(PathClass.shape_path, "tl_2019_us_county.shp")
+        PathClass.state_shapefile = os.path.join(PathClass.shape_path, "cb_2019_us_state_5m.shp")
+        PathClass.county_shapefile = os.path.join(PathClass.shape_path, "cb_2019_us_county_5m.shp")
         PathClass.state_covid_cases_file = os.path.join(PathClass.covid_path, "COVID-19-Cases-USA-By-State.csv")
         PathClass.state_covid_deaths_file = os.path.join(PathClass.covid_path, "COVID-19-Deaths-USA-By-State.csv")
         PathClass.county_covid_cases_file = os.path.join(PathClass.covid_path, "COVID-19-Cases-USA-By-County.csv")
